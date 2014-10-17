5f8ab4c68a68a59a9f5169479edb54c567137926
padjis <padjinouemmanuel@gmail.com>
2014-10-17 18:01:38 +0100

    HTML 5 part initialized

---
+++ GitBootStrap.GUI/js/myscript.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function(){
+    $("#hide").click(function () {
+        $("p").hide();
+    });
+
+    $("#show").click(function () {
+        $("p").show();
+    });
+
+    $("button").click(function () {
+        $("p").toggle();
+    });
+});
+
+
+
